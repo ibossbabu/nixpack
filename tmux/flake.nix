37af110e127d3003ba91d0645ed79d0f8befb617
ibossbabu <boss160250@gmail.com>
2025-12-20 19:57:46 +0700

    add tmux package

---
+++ tmux/flake.nix
@@ -0,0 +1,30 @@
+{
+  description = "tmux";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = inputs @ {
+    self,
+    nixpkgs,
+    flake-utils,
+    ...
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {inherit system;};
+        tmuxModule = import ./tmux.nix {
+          inherit inputs pkgs;
+        };
+      in {
+        package = {
+          tmux = tmuxModule.package;
+        };
+        apps = {
+          tmux = tmuxModule;
+        };
+      }
+    );
+}
