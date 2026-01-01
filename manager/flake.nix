df170a5c74a2f7502383cc88b18ae1fa66e9e120
ibossbabu <boss160250@gmail.com>
2026-01-01 22:52:54 +0700

    add pack for linux

--- manager/flake.nix
+++ manager/flake.nix
@@ -15,11 +15,14 @@
     flake-utils.lib.eachDefaultSystem (system: let
       pkgs = import nixpkgs {inherit system;};
 
-      targets = [
-        "$HOME/nixpack/neovim"
-        "$HOME/nixpack/tmux"
-      ];
-
+      targets =
+        [
+          "$HOME/nixpack/neovim"
+          "$HOME/nixpack/tmux"
+        ]
+        ++ pkgs.lib.optionals pkgs.stdenv.isLinux [
+          ## For linux
+        ];
       install = pkgs.writeShellScript "install-nix-flakes" ''
         #!/usr/bin/env bash
         TARGETS=(${builtins.concatStringsSep " " targets})
@@ -42,11 +45,11 @@
         nix profile upgrade --all
       '';
     in {
-       packages = {
-          install = install;
-          update = update;
-          default = install;
-        };
+      packages = {
+        install = install;
+        update = update;
+        default = install;
+      };
       apps = {
         install = {
           type = "app";
