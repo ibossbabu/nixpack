df4a6512c15c6717c7c70fcca7981b88376a05b8
ibossbabu <boss160250@gmail.com>
2026-01-01 22:52:00 +0700

    update treesitter fix

--- neovim/nix/nvim/plugin/treesitter.lua
+++ neovim/nix/nvim/plugin/treesitter.lua
@@ -1,17 +1,10 @@
 --vim.cmd.packadd 'nvim-treesitter'
 --vim.opt.runtimepath:prepend(vim.fn.expand("~/.local/share/nvim/treesitter"))
+vim.opt.runtimepath:remove(vim.fn.expand("~/.local/share/nvim/site"))
 vim.defer_fn(function()
-  require('nvim-treesitter.configs').setup {
-    --parser_install_dir = vim.fn.expand("~/.local/share/nvim/treesitter/parser/"),
-    modules = {},
-    ignore_install = {},
-    ensure_installed = {},
-    sync_install = false
-    , auto_install = false,
-    highlight = {
-      enable = true,
-      additional_vim_regex_highlighting = false,
-    },
-    indent = { enable = false, },
-  }
+  vim.api.nvim_create_autocmd("FileType", {
+    callback = function(args)
+      pcall(vim.treesitter.start, args.buf)
+    end,
+  })
 end, 1)
