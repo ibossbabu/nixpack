cc96b3c86c41149adfd20050282397e9a181f900
ibossbabu <boss160250@gmail.com>
2026-01-12 22:49:48 +0700

    update treesitter config

--- neovim/nix/nvim/plugin/treesitter.lua
+++ neovim/nix/nvim/plugin/treesitter.lua
@@ -1,10 +1,34 @@
---vim.cmd.packadd 'nvim-treesitter'
---vim.opt.runtimepath:prepend(vim.fn.expand("~/.local/share/nvim/treesitter"))
-vim.opt.runtimepath:remove(vim.fn.expand("~/.local/share/nvim/site"))
-vim.defer_fn(function()
-  vim.api.nvim_create_autocmd("FileType", {
-    callback = function(args)
-      pcall(vim.treesitter.start, args.buf)
-    end,
-  })
-end, 1)
+local ts = vim.treesitter
+
+local ts_enable = function(buf, lang)
+  if vim.b[buf].ts_enabled then
+    return
+  end
+
+  local ok, hl = pcall(ts.query.get, lang, 'highlights')
+  if ok and hl then
+    vim.b[buf].ts_enabled = true
+    ts.start(buf)
+  end
+end
+
+vim.api.nvim_create_autocmd('FileType', {
+  desc = 'enable treesitter',
+  callback = function(event)
+    local ft = event.match
+    local buf = event.buf
+
+    vim.schedule(function()
+      if not vim.api.nvim_buf_is_valid(buf) then
+        return
+      end
+
+      local lang = vim.treesitter.language.get_lang(ft)
+      if not lang then
+        return
+      end
+
+      ts_enable(buf, lang)
+    end)
+  end,
+})
