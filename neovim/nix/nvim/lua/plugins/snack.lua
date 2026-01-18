187f5037ed29503bd3b3beba79a435933323581a
ibossbabu <boss160250@gmail.com>
2026-01-18 16:14:38 +0700

    add gitdiff keymap

--- neovim/nix/nvim/lua/plugins/snack.lua
+++ neovim/nix/nvim/lua/plugins/snack.lua
@@ -3,13 +3,14 @@ return {
     "snack.picker",
     keys = {
       { "<leader>sc", function() Snacks.picker.commands() end, desc = "Commands" },
-      { "<leader>sf", function() Snacks.picker.smart() end, desc = "Smart Find Files" },
-      { "<leader>sg", function() Snacks.picker.lines() end, desc = "Buffer Lines" },
-      { "<leader>sl", function() Snacks.picker.grep() end, desc = "Grep" },
-      { "<leader>sr", function() Snacks.picker.resume() end, desc = "Resume" },
-      { "<leader>so", function() Snacks.picker.recent() end, desc = "Recent" },
+      { "<leader>sf", function() Snacks.picker.smart() end,    desc = "Smart Find Files" },
+      { "<leader>sg", function() Snacks.picker.lines() end,    desc = "Buffer Lines" },
+      { "<leader>sl", function() Snacks.picker.grep() end,     desc = "Grep" },
+      { "<leader>sr", function() Snacks.picker.resume() end,   desc = "Resume" },
+      { "<leader>so", function() Snacks.picker.recent() end,   desc = "Recent" },
       --ZEN mode
-      { "<leader>ls", function() Snacks.zen() end, desc = "ZEN" },
+      { "<leader>ls", function() Snacks.zen() end,             desc = "ZEN" },
+      { "<leader>ty", function() Snacks.picker.git_diff() end, desc = "Diff" },
     },
   },
 }
